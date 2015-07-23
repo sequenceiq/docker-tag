7a1fc96ba5635237bee2aa346d4cc6e2c55ef0b4
Richard Doktorics <rdoktorics@hortonworks.com>
2015-07-23 19:09:10 +0200

    Update commit-tag.sh

--- commit-tag.sh
+++ commit-tag.sh
@@ -37,7 +37,9 @@ git checkout $BRANCH
 npm install
 npm install semver -g
 ACTUAL_VERSION=$(echo $(npm version)|sed s/{//g |sed s/}//g|sed 's/ //g'| cut -d \, -f 1|sed s/$PROJECT://g|sed "s/'//g")
+echo $ACTUAL_VERSION
 TEST=npm version $(semver $ACTUAL_VERSION -i minor)-rc.0 
+echo $TEST
 git checkout -b $TEST 
   && npm version $TEST -m "increase version [ci skip]" 
   && git push -f origin $TEST 
