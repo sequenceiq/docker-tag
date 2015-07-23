ba306f93ed677be599a13b76bfa418440651696d
Richard Doktorics <rdoktorics@hortonworks.com>
2015-07-23 17:57:46 +0200

    Update commit-tag.sh

--- commit-tag.sh
+++ commit-tag.sh
@@ -37,4 +37,12 @@ git checkout $BRANCH
 npm install
 ACTUAL_VERSION=echo $(npm version)|sed 's/{//g' |sed 's/}//g'|sed 's/ //g'| cut -d \, -f 1|sed 's/$PROJECT://g'|sed "s/'//g"
 TEST=npm version $(semver $(semver $ACTUAL_VERSION -i minor) -i minor)-rc.0 
-git checkout -b $TEST && npm version $TEST -m "increase version [ci skip]" && git push -f origin $TEST && git push -f --tags && git checkout master && npm version $(semver $(semver $ACTUAL_VERSION -i minor) -i minor)-dev.0 -m "increase version [ci skip]" && git push -f origin $BRANCH && git push -f --tags && $POST_RUN_COMMAND
+git checkout -b $TEST 
+  && npm version $TEST -m "increase version [ci skip]" 
+  && git push -f origin $TEST 
+  && git push -f --tags 
+  && git checkout master 
+  && npm version $(semver $(semver $ACTUAL_VERSION -i minor) -i minor)-dev.0 -m "increase version [ci skip]" 
+  && git push -f origin $BRANCH 
+  && git push -f --tags 
+  && $POST_RUN_COMMAND
